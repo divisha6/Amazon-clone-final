fb01979c52115080d581655c717e2a2937e7a0cb
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-05-04 01:17:20 +0530

    completed lib

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:amazon_clone/layout/screen_layout.dart';
+import 'package:amazon_clone/providers/user_details_provider.dart';
+import 'package:amazon_clone/screens/sign_in_screen.dart';
+import 'package:amazon_clone/utils/color_themes.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+          apiKey: "AIzaSyALPW8YGFZJwT-DDYwlQNEmT0PnWtf3RwM",
+          authDomain: "clone-c395b.firebaseapp.com",
+          projectId: "clone-c395b",
+          storageBucket: "clone-c395b.appspot.com",
+          messagingSenderId: "344753163997",
+          appId: "1:344753163997:web:1fcc229913abfdf7885534"
+        ));
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(const AmazonClone());
+}
+
+class AmazonClone extends StatelessWidget {
+  const AmazonClone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => UserDetailsProvider())],
+      child: MaterialApp(
+        title: "Amazon Clone",
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light().copyWith(
+          scaffoldBackgroundColor: backgroundColor,
+        ),
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, AsyncSnapshot<User?> user) {
+              if (user.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.orange,
+                  ),
+                );
+              } else if (user.hasData) {
+                return const ScreenLayout();
+                //return const SellScreen();
+              } else {
+                return const SignInScreen();
+              }
+            }),
+      ),
+    );
+  }
+}
